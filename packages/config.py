42b575e156f675044c8c0afc78681500aecc375d
Denes Farago <denes.farago@avl.com>
2024-07-18 09:20:37 +0200

    Improved config.py and added pylint rc file to the project.

--- packages/config.py
+++ packages/config.py
@@ -7,18 +7,18 @@ import configparser
 
 class Config:
     """
-Loads a config file and gets config values
+    Loads a config file and gets config values
     """
 
     def __init__(self, path):
         if not os.path.exists(path):
-            raise Exception(f"Config file was not found: {os.path.abspath(path)}")
+            raise FileNotFoundError(f"Config file was not found: {os.path.abspath(path)}")
         self._config = configparser.ConfigParser()
         self._config.read(path)
 
     def get_entry(self, section, key):
         """
-Reads a config value
+        Reads a config value
         :param section: Section of the config file
         :param key: Name of the config entry to read
         :return: The config entry
@@ -26,14 +26,12 @@ Reads a config value
         if section in self._config:
             if key in self._config[section]:
                 return self._config[section][key]
-            else:
-                raise Exception(f"Key '{key}' was not found in section '{section}' of the config file")
-        else:
-            raise Exception(f"Section '{section}' was not found in the config file")
+            raise KeyError(f"Key '{key}' was not found in section '{section}' of the config file")
+        raise KeyError(f"Section '{section}' was not found in the config file")
 
     def get_float(self, section, key):
         """
-Reads a config value and converts it to a float. Throws an error if the value cannot be interpreted as a float.
+        Reads a config value and converts it to a float. Throws an error if the value cannot be interpreted as a float.
         :param section: Section of the config file
         :param key: Name of the config entry to be read as a float
         :return: The config entry converted to float
@@ -41,12 +39,13 @@ Reads a config value and converts it to a float. Throws an error if the value ca
         entry = self.get_entry(section, key)
         try:
             return float(entry)
-        except ValueError:
-            raise Exception(f"Invalid config entry: {entry} in section '{section}', key '{key}'. The type of the value has to be float")
+        except ValueError as exc:
+            raise ValueError(f"Invalid config entry: {entry} in section '{section}', key '{key}'. "
+                             f"The type of the value has to be float") from exc
 
     def get_int(self, section, key):
         """
-Reads a config value and converts it to an int. Throws an error if the value cannot be interpreted as an int.
+        Reads a config value and converts it to an int. Throws an error if the value cannot be interpreted as an int.
         :param section: Section of the config file
         :param key: Name of the config entry to read as an int
         :return: The config entry converted to int
@@ -54,5 +53,6 @@ Reads a config value and converts it to an int. Throws an error if the value can
         entry = self.get_entry(section, key)
         try:
             return int(entry)
-        except ValueError:
-            raise Exception(f"Invalid config entry: '{entry}' for section '{section}', key '{key}'. The type of the value has to be an int")
+        except ValueError as exc:
+            raise ValueError(f"Invalid config entry: '{entry}' for section '{section}', key '{key}'. "
+                             f"The type of the value has to be an int") from exc
